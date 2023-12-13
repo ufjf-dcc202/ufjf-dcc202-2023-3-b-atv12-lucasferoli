cdf14dd8b9965b5b0414fb19f6cc86caf657ff4d
lucasferolis <lucasferoliveira.goval@gmail.com>
2023-12-12 23:30:27 -0300

    Criada as demais funcoes do main.js, testes passados.

--- main.js
+++ main.js
@@ -2,3 +2,43 @@ import { getEstoque, transacaoNoEstoque, limpaEstoque } from "./estoque.js";
 
 const olJoao = document.querySelector("#joao");
 const olMaria = document.querySelector("#maria");
+
+document.entrada.addEventListener('submit', leFormulario);
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('limpar').addEventListener('click', () => {
+        limpaEstoque();
+        atualizaTela();});});
+atualizaTela();
+
+function leFormulario(event)
+    {event.preventDefault(event)
+    const quantidade = document.entrada.quantidade.valueAsNumber;
+    const fruta = document.entrada.fruta.value;
+    const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
+    
+    console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
+
+    transacaoNoEstoque(origem, destino, fruta, quantidade);
+    atualizaTela();}
+
+function atualizaTela(){
+    const estoque = getEstoque();
+    olJoao.innerHTML = "";
+    olMaria.innerHTML = "";
+    document.entrada.quantidade.value = 1;
+    document.entrada.fruta.value = "maca";
+
+    if (estoque.joao && estoque.joao.length > 0) {
+        preencheLista(olJoao, estoque.joao);}
+
+    if (estoque.maria && estoque.maria.length > 0) {
+        preencheLista(olMaria, estoque.maria);}}
+
+function preencheLista(lista, estoqueDaPessoa){
+    lista.textContent = "";
+    for(let i = 0; i < estoqueDaPessoa.length; i++)
+        {const monte = estoqueDaPessoa[i];
+        const li = document.createElement('li');
+        li.textContent = `${monte.tipo}: ${monte.quantidade}`;
+        lista.appendChild(li);}}
